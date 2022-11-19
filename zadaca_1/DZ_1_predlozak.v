4fad25d44bd728a3d01b4b2d9d4f1d95b2b00c13
lbrodar5 <105885981+lbrodar5@users.noreply.github.com>
2022-11-19 23:53:38 +0100

    Add files via upload

---
+++ zadaca_1/DZ_1_predlozak.v
@@ -0,0 +1,214 @@
+Set Implicit Arguments.
+Require Import List.
+Import ListNotations.
+Require Import Lia.
+
+(* Bit *)
+
+Inductive B : Type :=
+  | O : B
+  | I : B.
+
+Definition And (x y : B) : B :=
+match x with
+  | O => O
+  | I => y
+end.
+
+Definition Or (x y : B) : B :=
+match x with
+  | O => y
+  | I => I
+end.
+
+Definition Not (x : B) : B :=
+match x with
+  | O => I
+  | I => O
+end.
+
+Definition Add (x y c : B) : B :=
+match x, y with
+  | O, O => c
+  | I, I => c
+  | _, _ => Not c
+end.
+
+Definition Rem (x y c : B) : B :=
+match x, y with
+  | O, O => O
+  | I, I => I
+  | _, _ => c
+end.
+
+(* List *)
+
+Fixpoint AndL (x y : list B) : list B :=
+match x, y with
+| [], _ => []
+| _, [] => []
+| x :: xs, y :: ys => And x y :: AndL xs ys
+end.
+
+Fixpoint OrL (x y : list B) : list B :=
+match x, y with
+| [], _ => []
+| _, [] => []
+| x :: xs, y :: ys => Or x y :: OrL xs ys
+end.
+
+Fixpoint NotL (x : list B) : list B :=
+match x with
+  | [] => []
+  | x :: xs => Not x :: NotL xs
+end.
+
+Fixpoint AddLr (x y : list B) (c : B) : list B :=
+match x, y with
+| [], _ => []
+| _, [] => []
+| x :: xs, y :: ys => Add x y c :: AddLr xs ys (Rem x y c)
+end.
+
+Definition AddL (x y : list B) : list B := rev (AddLr (rev x) (rev y) O).
+
+Fixpoint IncLr (x : list B) (c : B) : list B :=
+match x with
+| [] => []
+| x :: xs => Add x I c :: IncLr xs (Rem x I c)
+end.
+
+Definition IncL (x : list B) : list B := rev (IncLr (rev x) O).
+
+(* ALU *)
+
+Definition flag_zx (f : B) (x : list B) : list B :=
+match f with
+| I => repeat O (length x)
+| O => x
+end.
+
+Definition flag_nx (f : B) (x : list B) : list B :=
+match f with
+| I => NotL x
+| O => x
+end.
+
+Definition flag_f (f : B) (x y : list B) : list B :=
+match f with
+| I => AddL x y
+| O => AndL x y
+end.
+
+Definition ALU (x y : list B) (zx nx zy ny f no : B) : list B := 
+  flag_nx no (flag_f f (flag_nx nx (flag_zx zx x)) (flag_nx ny (flag_zx zy y))).
+
+(* Teoremi *)
+
+Lemma ALU_zero (x y : list B) :
+  length x = length y -> ALU x y I O I O I O = repeat O (length x).
+Proof. Abort.
+
+Lemma ALU_minus_one (x y : list B) : 
+  length x = length y -> ALU x y I I I O I O = repeat I (length x).
+Proof. Abort.
+
+Lemma ALU_x (x y : list B) : 
+  length x = length y -> ALU x y O O I I O O = x.
+Proof. Abort.
+
+Lemma ALU_y (x y : list B) : 
+  length x = length y -> ALU x y I I O O O O = y.
+Proof. Abort.
+
+Lemma ALU_Not_x (x y : list B) : 
+  length x = length y -> ALU x y O O I I O I = NotL x.
+Proof. Abort.
+
+Lemma ALU_Not_y (x y : list B) : 
+  length x = length y -> ALU x y I I O O O I = NotL y.
+Proof. Abort.
+
+Lemma ALU_Add (x y : list B) : 
+  length x = length y -> ALU x y O O O O I O = AddL x y.
+Proof. Abort.
+
+(* DZ *)
+
+Lemma ALU_And (x y : list B) : 
+  length x = length y -> ALU x y O O O O O O = AndL x y.
+Proof.
+now simpl. 
+Qed.
+
+Lemma ALU_Or (x y : list B) : 
+  length x = length y -> ALU x y O I O I O I = OrL x y.
+Proof.
+revert x y. simpl.  induction x. try now simpl.
+  -destruct y.
+  --now simpl.
+  --simpl. intros. rewrite IHx.
+  ---destruct a,b; try now simpl.
+  ---Search(S _ = S _ ). apply eq_add_S. apply H.
+Qed.
+
+Lemma ALU_One (n : nat) (x y : list B) :
+  length x = n /\ length y = n /\ n <> 0 -> ALU x y I I I I I I = repeat O (pred n) ++ [I].
+Proof.
+simpl. intros.
+
+assert(A: (forall (x : list B), NotL (repeat O (length x)) = repeat I (length x) )).
+{
+  intros. induction x0.
+  -now simpl.
+  -simpl. now rewrite IHx0.
+}
+rewrite A. rewrite A.
+destruct H, H0. 
+rewrite H, H0.
+
+ assert (H_revL_1 : forall (b:B) (n:nat),repeat b n ++ [b] = b :: repeat b n).
+ {
+  intros. induction n0.
+    -now simpl.
+    - simpl. now rewrite IHn0.
+ }
+
+ assert (H_revL_2 : forall (b:B) (n:nat),rev(repeat b n) = repeat b n).
+ {
+  intros. induction n0.
+  -now simpl.
+  - simpl. rewrite IHn0. apply H_revL_1.
+ }
+
+unfold AddL.
+rewrite H_revL_2.
+
+assert(C: forall (a : nat), (AddLr (repeat I a) (repeat I a) I) = repeat I a).
+{
+intros. induction a.
+  -now simpl.
+  -simpl. rewrite IHa. reflexivity.
+}
+
+induction n.
+  -now simpl.
+  -simpl. rewrite C. rewrite H_revL_2.
+
+assert(B: forall (a : nat), NotL (repeat I a ++ [O]) = repeat O a ++ [I]).
+{
+  intros. induction a.
+  -now simpl.
+  -simpl. rewrite IHa. reflexivity.
+} 
+
+apply B.
+
+Qed.
+
+
+
+
+
+
+
---
+++ zadaca_1/zadatak1/zadatak1.v
@@ -0,0 +1,18 @@
+Require Import Bool.
+
+Notation " ! b " := (negb b) (at level 0).
+
+(* a *)
+Goal forall (x y : bool), 
+    ! (x && y) || (!x && y) || (!x && !y) = !(x && y).
+Proof.
+    now destruct x, y.
+Qed.
+
+(* b *)
+Goal forall (x y z: bool), 
+    !(!x && y && !z) && !(x && y && z) && (x && !y && !z) 
+    =  x && !(z || y).
+Proof.
+    now destruct x, y, z. 
+Qed.
